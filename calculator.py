41cde3154ffe5cc1b2fa4ce630b2604726511e0b
Ray-Jay Peart <119546415+RJay-P@users.noreply.github.com>
2025-11-16 13:02:21 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,7 +5,24 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+import math
+
 def add(a, b): 
-    pass
+    return a + b
 
+def subtract(a, b): 
+    return a - b
 
+def multiply(a, b): 
+    return a * b
+
+def divide(a, b): 
+    raise ZeroDivisionError if a == 0 
+    return b / a    
+    
+def logarithm(a, b): 
+    return loga(b)
+    # use math library/raise ValueError
+
+def exponent(a, b): 
+    return ab
